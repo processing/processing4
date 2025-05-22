76f1403c704d607de17782fd3fb6a54b7db749ef
jishnunambiarr <jishnunambiar03@gmail.com>
2025-05-22 23:15:19 +0200

    Updated build.gradle.kts with manual Batik download.

--- java/libraries/svg/build.gradle.kts
+++ java/libraries/svg/build.gradle.kts
@@ -71,17 +71,28 @@ val downloadBatik by tasks.registering {
 
 }
 
+val coreJar = file("../../../core/library/core.jar")
+
+tasks.register("checkCore") {
+    doFirst {
+
+        if (!coreJar.exists()) {
+            throw GradleException("Missing core.jar at $coreJar. Please build the core module first.")
+        }
+    }
+}
+
 
 
 dependencies {
-    implementation(project(":core"))
+    implementation(files(coreJar))
     implementation(files(batikJar))
 
 }
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
