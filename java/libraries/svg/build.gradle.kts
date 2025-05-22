ea204277b2c22c54b6d3c989dd5872d6d8cd30b8
jishnunambiarr <jishnunambiar03@gmail.com>
2025-05-22 18:39:31 +0200

    Use Maven dependency for Batik in build.gradle.kts

--- java/libraries/svg/build.gradle.kts
+++ java/libraries/svg/build.gradle.kts
@@ -1,1 +1,32 @@
-ant.importBuild("build.xml")
+plugins {
+    id("java")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation("org.apache.xmlgraphics:batik-all:1.19")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+sourceSets {
+    main {
+        java.srcDirs("src")
+    }
+}
+
+tasks.jar {
+    archiveBaseName.set("svg")
+    destinationDirectory.set(file("library"))
+}
+
+tasks.clean {
+    delete("bin", "library/svg.jar")
+}
