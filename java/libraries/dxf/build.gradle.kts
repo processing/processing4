e37e67d0f57bdd3ffef6145089841d20c9e34380
Moon D. <moon@softmoon.world>
2025-09-11 15:22:19 -0400

    [Ant->Gradle] Dxf

--- java/libraries/dxf/build.gradle.kts
+++ java/libraries/dxf/build.gradle.kts
@@ -1,1 +1,38 @@
-ant.importBuild("build.xml")
+plugins{
+    java
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs("src")
+        }
+    }
+}
+repositories{
+    mavenCentral()
+    maven("https://jogamp.org/deployment/maven/")
+}
+
+dependencies{
+    compileOnly(project(":core"))
+
+}
+
+tasks.register<Copy>("createLibrary"){
+    dependsOn("jar")
+    into(layout.buildDirectory.dir("library"))
+
+    from(layout.projectDirectory){
+        include ("library.properties")
+        include("examples/**")
+    }
+
+    from(configurations.runtimeClasspath){
+        into("library")
+    }
+
+    from(tasks.jar) {
+        into("library")
+    }
+}
