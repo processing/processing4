f36e5b2d83a1c431b8ffe9bb715d9f2a3bae1429
Madhav Majumdar <161720210+madhav2348@users.noreply.github.com>
2025-10-07 02:40:19 +0530

    Migration of the `serial` library to Gradle (#1106)
    * Update build.gradle.kts
    
    * Update build.gradle.kts
    
    * fix build.gradle.kts
    
    * Resolving issue 1099
    
    - Add settings.gradle.kts
    - Update build.gradle.kts
    
    * foojay-resolver-convention updated to latest version
    
    * update migration code and revert unnecessary commits

--- java/libraries/serial/build.gradle.kts
+++ java/libraries/serial/build.gradle.kts
@@ -1,1 +1,36 @@
-ant.importBuild("build.xml")
+plugins {
+    java
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs("src")
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(project(":core"))
+    // TODO: https://github.com/java-native/jssc
+    implementation(files("library/jssc.jar"))
+}
+
+tasks.register<Copy>("createLibrary") {
+    dependsOn("jar")
+    into(layout.buildDirectory.dir("library"))
+    from(layout.projectDirectory) {
+        include("library.properties")
+        include("examples/**")
+    }
+    from(configurations.runtimeClasspath) {
+        into("library")
+    }
+    from(tasks.jar) {
+        into("library")
+    }
+}
