6224f8c64e9a5fdf9500cd381728ca84f5e5359d
Vaivaswat <vaivaswat2244@gmail.com>
2025-10-16 16:51:46 +0530

    deleting earlier files

--- visual-tests/src/main/java/processing/test/visual/ProcessingSketch.java
+++
@@ -1,9 +0,0 @@
-package processing.test.visual;
-
-import processing.core.PApplet;
-
-// Interface for user sketches
-public interface ProcessingSketch {
-    void setup(PApplet p);
-    void draw(PApplet p);
-}
