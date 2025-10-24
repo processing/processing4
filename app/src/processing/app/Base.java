5a6f7fa5bd34781e27739b78e17ec039446193f6
Stef Tervelde <stef@steftervelde.nl>
2025-10-24 11:32:23 +0200

    Clean up handlePrefs method by removing comments
    Removed commented-out code for preferences frame initialization.

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -2191,10 +2191,6 @@ public class Base {
    * Show the Preferences window.
    */
   public void handlePrefs() {
-//    if (preferencesFrame == null) {
-//      preferencesFrame = new PreferencesFrame(this);
-//    }
-//    preferencesFrame.showFrame();
       PreferencesKt.show();
   }
 
