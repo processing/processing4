e24febe1d4dafabcfb47941d2ce1dea2ded9b18d
Stef Tervelde <stef@steftervelde.nl>
2026-01-15 12:54:04 +0100

    Reset defaults by setting value rather than remove
    Initially reset to defaults was set up by removing the key from the preferences but that didn't work well upstream, changing the value to default instead.

--- app/src/processing/app/Preferences.kt
+++ app/src/processing/app/Preferences.kt
@@ -38,6 +38,9 @@ class ReactiveProperties : Properties() {
     operator fun set(key: String, value: String) {
         setProperty(key, value)
     }
+    fun remove() {
+        TODO("Not yet implemented")
+    }
 }
 
 /*
