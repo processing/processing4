aaa3f75362bc9fa52e4b58c75f1608cc1c2df952
Stef Tervelde <stef@steftervelde.nl>
2026-01-16 15:52:40 +0100

    Create PreferencesEvents to glue into base and other deps (#1386)
    To avoid creating dependencies within Preferences.kt, create an event structure that will listen to changes triggered by saving the Preferences.

--- app/src/processing/app/Preferences.kt
+++ app/src/processing/app/Preferences.kt
@@ -1,12 +1,9 @@
 package processing.app
 
 import androidx.compose.runtime.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.dropWhile
-import kotlinx.coroutines.launch
 import processing.utils.Settings
 import java.io.File
 import java.io.InputStream
@@ -134,10 +131,9 @@ fun PreferencesProvider(content: @Composable () -> Unit) {
                             .joinToString("\n") { (key, value) -> "$key=$value" }
                             .toByteArray()
                     )
-                    
-                    // Reload legacy Preferences
-                    Preferences.init()
                     output.close()
+
+                    PreferencesEvents.updated()
                 }
             }
     }
@@ -205,4 +201,19 @@ fun watchFile(file: File): Any? {
         }
     }
     return event
+}
+
+class PreferencesEvents {
+    companion object {
+        val updatedListeners = mutableListOf<Runnable>()
+
+        @JvmStatic
+        fun onUpdated(callback: Runnable) {
+            updatedListeners.add(callback)
+        }
+
+        fun updated() {
+            updatedListeners.forEach { it.run() }
+        }
+    }
 }
