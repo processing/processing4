f46bd75ccb297b3df2351fb41f159c4b216ea321
Stef Tervelde <stef@steftervelde.nl>
2026-02-03 19:34:31 +0100

    Publish Processing Gradle plugin
    Add a publish-gradle job to the release workflow to publish Processing libraries to the Gradle Plugin Portal using gradle publish with required secrets and version/group env vars. Update gradle/plugins/library/build.gradle.kts to use the com.gradle.plugin-publish plugin, provide plugin metadata (website, vcsUrl, displayName, description, tags) and make the plugin id dynamic ("$group.library"). These changes enable automated publishing of the Gradle plugin with the metadata required by the portal.

--- gradle/plugins/library/build.gradle.kts
+++ gradle/plugins/library/build.gradle.kts
@@ -1,12 +1,17 @@
 plugins {
-    `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "2.0.0"
     kotlin("jvm") version "2.2.20"
 }
 
 gradlePlugin {
+    website = "https://processing.org/"
+    vcsUrl = "https://github.com/processing/processing4"
     plugins {
         create("processing.library") {
-            id = "org.processing.library"
+            id = project.properties.getOrElse("publishingGroup", { "org.processing" }).toString() + ".library"
+            displayName = "Processing Library Plugin"
+            description = "Gradle plugin for building Processing libraries"
+            tags = listOf("processing", "library", "dsl")
             implementationClass = "ProcessingLibraryPlugin"
         }
     }
