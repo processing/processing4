62259029ac1b26180a6cdbe45cf194bea9ce3e1b
Elijah Baron <elijahjbaron96@gmail.com>
2026-04-01 13:30:26 -0400

    Added first test! added tons of comments.

--- core/test/processing/core/PFontTest.java
+++ core/test/processing/core/PFontTest.java
@@ -1,7 +1,15 @@
 package processing.core;
 
+import java.awt.Font;         //Javas built in font class
+import org.junit.Test;        //Using Junit4 test implementation
+import static org.junit.Assert.assertEquals;    //To compare expected vs actual values
+
 public class PFontTest {
 
+    @Test
+    public void constructor_setsSizeCorrectly() {
+        Font awtFont = new Font("Dialog", Font.PLAIN, 16);  //Truth, what PFont size should be
+        PFont font = new PFont(awtFont, true, null); //call to constructor w specific values
+        assertEquals(16, font.getSize()); //actual test, expects 16 compares against actual value
+    }
 }
-
-//Write Tests HERE!
