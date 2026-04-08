9789ddc28b56dc9b95154286d959f12c97c0244f
Jocelyn Francisco <franciscoj@emmanuel.edu>
2026-04-08 14:29:48 -0400

    added MessagesTest.kt

---
+++ app/test/processing/app/MessagesTest.kt
@@ -0,0 +1,64 @@
+package processing.app
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class MessagesTest {
+    @Test
+    fun showMessage() {
+
+    }
+
+    @Test
+    fun showWarning() {
+    }
+
+    @Test
+    fun testShowWarning() {
+    }
+
+    @Test
+    fun testShowWarning1() {
+    }
+
+    @Test
+    fun showWarningTiered() {
+    }
+
+    @Test
+    fun showError() {
+    }
+
+    @Test
+    fun showTrace() {
+    }
+
+    @Test
+    fun showYesNoQuestion() {
+    }
+
+    @Test
+    fun showCustomQuestion() {
+    }
+
+    @Test
+    fun log() {
+    }
+
+    @Test
+    fun testLog() {
+    }
+
+    @Test
+    fun logf() {
+    }
+
+    @Test
+    fun err() {
+    }
+
+    @Test
+    fun testErr() {
+    }
+
+}
