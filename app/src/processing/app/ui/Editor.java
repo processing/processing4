341463cbb4b3f7bffd29609c20806129c6eaced7
smarasca5 <smarasca5@gmail.com>
2026-04-08 14:36:21 -0400

    testing testing

--- app/src/processing/app/ui/Editor.java
+++ app/src/processing/app/ui/Editor.java
@@ -20,7 +20,7 @@
   along with this program; if not, write to the Free Software Foundation,
   Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-
+// comment comment hi hi hi
 package processing.app.ui;
 
 import com.formdev.flatlaf.util.SystemInfo;
