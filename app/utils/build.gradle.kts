766c6e0ce73774bd16d92d44bd5304d31180031a
Chaitanya-970 <chaitanyabh97@example.com>
2026-09-18 14:29:31 +0530

    Configure Maven publishing for app/utils

--- app/utils/build.gradle.kts
+++ app/utils/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.vanniktech.maven.publish.SonatypeHost
 plugins {
     id("java")
     alias(libs.plugins.mavenPublish)
@@ -18,6 +19,39 @@ publishing{
         maven {
             name = "App"
             url = uri(project(":app").layout.buildDirectory.dir("resources-bundled/common/repository").get().asFile.absolutePath)
+        }
+    }
+}
+    mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+
+    if (project.hasProperty("signing.keyId") || project.hasProperty("signingInMemoryKey"))
+        signAllPublications()
+
+    pom {
+        name.set("Processing Utilities")
+        description.set("Processing Utilities")
+        url.set("https://processing.org")
+        licenses {
+            license {
+                name.set("LGPL")
+                url.set("https://www.gnu.org/licenses/lgpl-2.1.html")
+            }
+        }
+        developers {
+            developer {
+                id.set("steftervelde")
+                name.set("Stef Tervelde")
+            }
+            developer {
+                id.set("benfry")
+                name.set("Ben Fry")
+            }
+        }
+        scm {
+            url.set("https://github.com/processing/processing4")
+            connection.set("scm:git:git://github.com/processing/processing4.git")
+            developerConnection.set("scm:git:ssh://git@github.com/processing/processing4.git")
         }
     }
 }
